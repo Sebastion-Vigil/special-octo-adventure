df12127ace8c83b098e5414858dd22b7fb0d9f04
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-01-03 22:19:10 -0600

    LetGodBlessTrump sheet complete

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Flag from './Flag.js'
+
+import '../css/App.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='App'>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+        <Flag/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Flag.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../css/Flag.css'
+
+class Flag extends React.Component {
+  render () {
+    return (
+      <div className='flag'>
+        <div className='text header'>LGBTQ</div>
+        <div className='text'>Let God Bless Trump (Quickly)</div>
+      </div>
+    )
+  }
+}
+
+export default Flag
