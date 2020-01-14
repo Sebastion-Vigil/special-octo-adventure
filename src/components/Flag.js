2b0d3c056e7a258adbd9cebaca19b84867e2d3da
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-01-14 04:57:05 -0600

    mimus-polyglottos

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import '../css/Flag.css'
 
 class Flag extends React.Component {
   render () {
     return (
-      <div className='flag'>
+      <div className="flag-wrapper">
+        <div className='flag'></div>
         <div className='text'>Let God Bless Trump Quickly</div>
       </div>
     )
